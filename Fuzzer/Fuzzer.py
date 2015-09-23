e9570f235b81f234f472cf121aa0aad2d9e9905d
rnn7726 <rnn7726@rit.edu>
2015-09-22 23:04:37 -0400

    update
    update

--- Fuzzer/Fuzzer.py
+++ Fuzzer/Fuzzer.py
@@ -1,16 +1,12 @@
 import requests
+import textwrap
+import operator
 from lxml import html
 from urlparse import urlparse
 
 
-def text():
-    link = raw_input('Insert Link Here: ')
-    r = requests.get(link)
-    print (r.text)
-
-
-def getLinks():
-    # http://127.0.0.1:8080/bodgeit/
+#test purposes
+def links():
     link = raw_input('Insert Link Here: ')
     r = requests.get(link)
     reqhtml =  html.fromstring(r.text)
@@ -19,8 +15,26 @@ def getLinks():
         print (link + x)
 
 
+#test purposes
+def parse():
+    link = raw_input('Insert Link Here: ')
+    r = requests.get(link)
+    reqhtml =  html.fromstring(r.text)
+    ahreflist = reqhtml.xpath("//a/@href")
+    for x in ahreflist:
+        result = urlparse(x)
+        print (result)
+
+
+#test purposes
+def cookies():
+    session = requests.session()
+    link = raw_input('Insert Link To Look For Cookies: ')
+    session.get(link)
+    print('Cookies: ', requests.utils.dict_from_cookiejar(session.cookies))
+
+
 def getLinksAuth():
-    # http://127.0.0.1:8080/bodgeit/
     link = raw_input('Insert Link Here: ')
     r = requests.get(link, auth=('admin', 'password'))
     reqhtml =  html.fromstring(r.text)
@@ -29,25 +43,127 @@ def getLinksAuth():
         print (link + x)
 
 
-def parseURL():
-    link = raw_input('Insert Link Here: ')
-    r = requests.get(link)
+def getLinks(theLink):
+    r = requests.get(theLink)
+    reqhtml =  html.fromstring(r.text)
+    ahreflist = reqhtml.xpath("//a/@href")
+    for x in ahreflist:
+        print (theLink + x)
+
+
+def getInputs(theLink):
+    r = requests.get(theLink)
+    reqhtml =  html.fromstring(r.text)
+    inputnamelist = reqhtml.xpath("//input/@type")
+    for x in inputnamelist:
+        print ('Type: ' + x)
+
+
+def parseURL(theLink):
+    r = requests.get(theLink)
     reqhtml =  html.fromstring(r.text)
     ahreflist = reqhtml.xpath("//a/@href")
     for x in ahreflist:
         result = urlparse(x)
-        print result
+        print (result)
 
 
-#getLinks()
-getLinksAuth()
-#parseURL('http://127.0.0.1:8080/bodgeit/product.jsp')
-parseURL()
-#parseURL('http://127.0.0.1:8080/bodgeit/product.jsp?typeid=2')
+def getCookies(theLink):
+    session = requests.session()
+    session.get(theLink)
+    print('Cookies: ', requests.utils.dict_from_cookiejar(session.cookies))
 
 
+def custAuth(name):
+    if operator.eq(name, 'dvwa'):
+        # Fill in your details here to be posted to the login form.
+        payload = {
+            'username': 'admin',
+            'password': 'password',
+            'Login': 'Login'
+        }
+
+        # Use 'with' to ensure the session context is closed after use.
+        with requests.Session() as s:
+            p = s.post('http://127.0.0.1/dvwa/login.php', data=payload)
+            # print the html returned or something more intelligent to see if it's a successful login page.
+            print p.text
+    if operator.eq(name, 'bodgeit'):
+        # Fill in your details here to be posted to the login form.
+        payload = {
+            'username': 'test@thebodgeitstore.com',
+            'password': 'password',
+            'submit': 'Login'
+        }
+
+        # Use 'with' to ensure the session context is closed after use.
+        with requests.Session() as s:
+            p = s.post('http://127.0.0.1:8080/bodgeit/login.jsp', data=payload)
+            # print the html returned or something more intelligent to see if it's a successful login page.
+            print(p.text)
 
 
+def guessPages(txtFile, url):
+    extensions = ['.php', '.jsp', '.html']
+    with open(txtFile, 'r') as f:
+        for line in f:
+            for ext in extensions:
+                response = requests.get(url + line.strip() + ext)
+                if response.status_code < 400:
+                    print(url + line.strip() + ext)
+
+
+def discover():
+    print textwrap.dedent(
+        """
+fuzz [discover | test] url OPTIONS
+
+COMMANDS:
+  discover  Output a comprehensive, human-readable list of all discovered inputs to the system. Techniques include both crawling and guessing.
+  test      Discover all inputs, then attempt a list of exploit vectors on those inputs. Report potential vulnerabilities.
+
+OPTIONS:
+  --custom-auth=string     Signal that the fuzzer should use hard-coded authentication for a specific application (e.g. dvwa). Optional.
+
+  Discover options:
+    --common-words=file    Newline-delimited file of common words to be used in page guessing and input guessing. Required.
+
+  Test options:
+    --vectors=file         Newline-delimited file of common exploits to vulnerabilities. Required.
+    --sensitive=file       Newline-delimited file data that should never be leaked. It's assumed that this data is in the application's database (e.g. test data), but is not reported in any response. Required.
+    --random=[true|false]  When off, try each input to each page systematically.  When on, choose a random page, then a random input field and test all vectors. Default: false.
+    --slow=500             Number of milliseconds considered when a response is considered "slow". Default is 500 milliseconds
+        """
+    )
+    link = raw_input('Please Enter Command: ')
+    info = link.split()
+    command = info[0].lower()
+    url = info[1].lower()
+    options = info[2:]
+    for x in options:
+        if operator.contains(x, '--custom-auth='):
+            print ('Custom Authentication: ')
+            custAuth(x.split('=')[1])
+        if operator.contains(x, '--common-words='):
+            print('Searching Common Words: ')
+            guessPages(x.split('=')[1], url)
+    if operator.eq(command, 'discover'):
+        print('Discovering Links: ')
+        getLinks(url)
+        print('Parse URLs: ')
+        parseURL(url)
+        print('Discovering Cookies: ')
+        getCookies(url)
+        print('Get Inputs: ')
+        getInputs(url)
+    else:
+        print ('Nothing yet, save for test functionality')
+
+
+discover()
+
+
+# execfile('C:\\Users\\rnn7726\\PycharmProjects\\Fuzzer\\Fuzzer.py')
 
 
 
---
+++ Fuzzer/readme.txt
@@ -0,0 +1,34 @@
+Readme File
+
+Fuzzer Project Part 1 09/24/2015
+
+Description
+- The fuzzer application is a tool that is used to find potential weaknesses in a program. This is done by searching the surface of the program, and in this case a particular vulnerable web application.
+
+Running the fuzzer
+- The requirements for the fuzzer are as follows:
+	- (provided) The fuzzer itself
+	- (provided) File to check for different pages
+	- (available) SE Department machine with Python installed 
+
+- In the associated .zip file, you will find the above provided files, as well as the requests library for Python. 
+
+Step 1 - Run python 2.7.x in the command line (*Note: If you have multiple instances of python, you will need to make sure you are running 2.7.x, and not 3+, as there will be errors when doing so)
+
+	Example, in a lab machine, you are able to do so running the following command in the C:\Anaconda directory:
+	
+	python
+
+Step 2 - Once you have python running, locate the fuzzer.py file given and copy the url
+
+	Example, the location of the file is C:\Users\rnn7726\PycharmProjects\Fuzzer\Fuzzer.py
+	
+Step 3 - Add in an additional \ character where they appear in the url
+	
+	Example, the above becomes C:\\Users\\rnn7726\\PycharmProjects\\Fuzzer\\Fuzzer.py
+	
+Step 4 - Run the execfile('url') command, where url is the modified url from step 3
+
+	Example, execfile('C:\\Users\\rnn7726\\PycharmProjects\\Fuzzer\\Fuzzer.py')
+
+This will run the fuzzer and you will be able to execute commands within it. For this first part of the project, you will be able to use the discover command alongside two different options, --custom-auth= and --common-words=, according to the specifications set within the project description. 
